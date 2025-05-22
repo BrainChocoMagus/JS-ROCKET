9d49f4cf28a5be1ff3ec8b78ae1f98d40efe6d18
BrainChocoMagus <alfarorioja.lourdes@outlook.es>
2025-05-22 14:00:39 -0600

    JS BEGINIGN

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
 console.log('hello lulu');
+        var sayMyName =function(name){
+            alert('My names is '+name);
+
+
+
+        }
          var car= {
             make:'VW',
             type:'Polo',
             color:'blue',
             isTurnedOn:false,
             numberOfWheels:4,
-            seats[
+            seats:[
                 'seat 1','seat 2','seat 3','seat 4'
             ],
             turnOn:function(){
                 this.isTurnedOn=true;
-            }
+            },
             turnOff:function(){
                 this.isTurnedOn=false;
-            }
+            },
             fly:function(){
                 alert('fly');
                 this.isTurnedOn=false;
